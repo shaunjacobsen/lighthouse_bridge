21306130aaaff13ac36e88a7d9cf4e581b5a1415
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-03-23 15:05:16 -0500

    Added Redis store

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const redis = require('redis');
 require('./config/config');
 
 const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-instance");
   next();
 });
@@ -21,11 +19,7 @@ require('./modules/messages/routes.js')(app);
 require('./modules/sensors/routes.js')(app);
 
 // redis
-const client = redis.createClient();
 
-client.on('connect', () => {
-  console.log('connected to redis');
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server up on ${process.env.PORT}`);
