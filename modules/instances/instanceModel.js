9b382103e4b9bd0dcf08e4c69e803c2ffd108246
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-03-28 16:32:15 -0500

    generateAuthToken and findByToken added to Instance model

--- modules/instances/instanceModel.js
+++ modules/instances/instanceModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 let LighthouseInstance = new mongoose.Schema({
   name: { type: String, required: true, trim: true, minlength: 2 },
@@ -19,4 +20,36 @@ let LighthouseInstance = new mongoose.Schema({
   created: { type: Number, default: new Date().getTime() },
 });
 
-module.exports = { LighthouseInstance };
+LighthouseInstance.methods.generateAuthToken = function() {
+  let instance = this;
+  let access = 'auth';
+  let token = jwt
+    .sign({ _id: instance._id.toHexString(), access }, process.env.JWT_SECRET)
+    .toString();
+
+  instance.tokens = instance.tokens.concat({ access, token });
+  return instance.save().then(() => {
+    return token;
+  });
+};
+
+LighthouseInstance.statics.findByToken = function(token) {
+  let Instance = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return Instance.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth',
+  });
+};
+
+let Instance = mongoose.model('LighthouseInstance', LighthouseInstance);
+
+module.exports = { LighthouseInstance, Instance };
