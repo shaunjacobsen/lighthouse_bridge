bb009ae5cb48e8dd5e4419ab496f4ed508ba646c
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-03-28 23:55:42 -0500

    misspelled the word expires

--- modules/eventService/eventService.js
+++ modules/eventService/eventService.js
@@ -55,7 +55,7 @@ const invokeAction = async (recipient, action) => {
       title: action.title,
       shortTitle: action.shortTitle,
       body: action.body,
-      expiry: new Date().getTime() + action.expiry,
+      expires: new Date().getTime() + action.expiry,
       recipients: [recipient],
     };
     await message.send(messagePayload);
