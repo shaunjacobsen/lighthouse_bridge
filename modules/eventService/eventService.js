b5efe17a20ba36aa097248213d28681d527f6b3e
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-03-29 10:28:15 -0500

    Event Service placeholder text function created

--- modules/eventService/eventService.js
+++ modules/eventService/eventService.js
@@ -13,7 +13,7 @@ const eventService = {
       let subscribedDevices = await Device.find({})
       .where({ 'eventListeners.eventType': eventType })
       .where({ 'eventListeners.originator': originator })
-      .select('_id eventListeners.actions eventListeners.eventTrigger');
+      .select('_id eventListeners.actions eventListeners.eventTrigger eventListeners.originator');
       let relevantTriggers;
       let triggerInvocationData = [];
       subscribedDevices.forEach(device => {
@@ -36,29 +36,57 @@ const eventService = {
 
 const handleTriggers = (triggerInvocationData) => {
   triggerInvocationData.forEach(invocation => {
-    invocation.triggers.forEach(trigger => {
-      invokeActions(invocation.device, trigger.actions);
+    invocation.triggers.forEach(async (trigger) => {
+      let originatingDevice;
+      try {
+        originatingDevice = await getDeviceInformation(trigger.originator.id);
+      } catch (error) {
+        console.log(error);
+      }
+      invokeActions(invocation.device, trigger.actions, { data: invocation.payload, originator: originatingDevice });
     });
     
   });
 }
 
-const invokeActions = (device, actions) => {
+const invokeActions = (device, actions, payload) => {
   actions.forEach(action => {
-    invokeAction(device._id, action);
+    invokeAction(device, action, payload);
   });
 }
 
-const invokeAction = async (recipient, action) => {
+const invokeAction = async (recipient, action, payload) => {
   if (action.type === 'message') {
     let messagePayload = {
       title: action.title,
       shortTitle: action.shortTitle,
-      body: action.body,
+      body: substitutePlaceholders(action.body, payload),
       expires: new Date().getTime() + action.expiry,
-      recipients: [recipient],
+      recipients: [recipient._id],
     };
     await message.send(messagePayload);
+  }
+}
+
+const substitutePlaceholders = (rawText, data) => {
+  const placeholderPattern = new RegExp('{{1}[A-z0-9\.]+}{1}', 'g');
+  let replacements = rawText.match(placeholderPattern);
+  let rawTextCopy = rawText.slice();
+  replacements.forEach(replacement => {
+    const extractedTextPattern = new RegExp('[A-z0-9\.]+');
+    const dataLookupPath = replacement.match(extractedTextPattern);
+    const replacementValue = keyPath.get(dataLookupPath[0]).getValueFrom(data);
+    rawTextCopy = rawTextCopy.replace(replacement, replacementValue);
+  });
+  return rawTextCopy;
+}
+
+const getDeviceInformation = async (id) => {
+  try {
+    let device = await Device.findById(id);
+    return device;
+  } catch (error) {
+    return error;
   }
 }
 
